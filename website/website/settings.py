56f4e7e733ef9e007da589f34398dd8eefd58dc5
Madhav Sharma <shekharmadhav03@gmail.com>
2020-06-21 17:41:23 +0530

    getting producn ready

--- website/website/settings.py
+++ website/website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,16 +27,15 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x+l*f^m^)getrektaokqda&)w2rz#2elol7r77d=#pbo9+i11ynlolm8d+_fbz'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = []
 
-RECAPTCHA_PUBLIC_KEY='6LfKrSKYZkAAAAAgetNpgAlQ2Phn3w8A9OMQ0tHHYG1_d'
-RECAPTCHA_PRIVATE_KEY='6LfKSvKYZAAAAAaJDzNKUsdaJX5p-dPF0ygine0PeI4sO'
-
+RECAPTCHA_PUBLIC_KEY= config('RECAPTCHA_PUBLIC_KEY')
+RECAPTCHA_PRIVATE_KEY=config('RECAPTCHA_PRIVATE_KEY')
 # Application definition
 
 INSTALLED_APPS = [
